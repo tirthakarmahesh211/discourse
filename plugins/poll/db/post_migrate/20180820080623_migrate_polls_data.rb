bc15b8b9e34cc91dc5b205523094ddcdb2866d45
Guo Xiang Tan <tgx_world@hotmail.com>
2018-10-09 13:25:00 +0800

    Don't rescue all errors.
    Not sure how we want to handle this but rescuing all
    means the migration will pass even when we have a typo.

--- plugins/poll/db/post_migrate/20180820080623_migrate_polls_data.rb
+++ plugins/poll/db/post_migrate/20180820080623_migrate_polls_data.rb
@@ -25,90 +25,86 @@ class MigratePollsData < ActiveRecord::Migration[5.2]
     SQL
 
     DB.query(sql).each do |r|
-      begin
-        existing_user_ids = User.where(id: r.votes.keys).pluck(:id).to_set
+      existing_user_ids = User.where(id: r.votes.keys).pluck(:id).to_set
 
-        # Poll votes are stored in a JSON object with the following hierarchy
-        #   user_id -> poll_name -> options
-        # Since we're iterating over polls, we need to change the hierarchy to
-        #   poll_name -> user_id -> options
+      # Poll votes are stored in a JSON object with the following hierarchy
+      #   user_id -> poll_name -> options
+      # Since we're iterating over polls, we need to change the hierarchy to
+      #   poll_name -> user_id -> options
 
-        votes = {}
-        r.votes.each do |user_id, user_votes|
-          # don't migrate votes from deleted/non-existing users
-          next unless existing_user_ids.include?(user_id.to_i)
+      votes = {}
+      r.votes.each do |user_id, user_votes|
+        # don't migrate votes from deleted/non-existing users
+        next unless existing_user_ids.include?(user_id.to_i)
 
-          user_votes.each do |poll_name, options|
-            votes[poll_name] ||= {}
-            votes[poll_name][user_id] = options
-          end
+        user_votes.each do |poll_name, options|
+          votes[poll_name] ||= {}
+          votes[poll_name][user_id] = options
         end
+      end
 
-        r.polls.values.each do |poll|
-          name = poll["name"].presence || "poll"
-          type = POLL_TYPES[(poll["type"].presence || "")[/(regular|multiple|number)/, 1] || "regular"]
-          status = poll["status"] == "open" ? 0 : 1
-          visibility = poll["public"] == "t" ? 1 : 0
-          close_at = (Time.zone.parse(poll["close"]) rescue nil)
-          min = poll["min"].to_i
-          max = poll["max"].to_i
-          step = poll["step"].to_i
+      r.polls.values.each do |poll|
+        name = poll["name"].presence || "poll"
+        type = POLL_TYPES[(poll["type"].presence || "")[/(regular|multiple|number)/, 1] || "regular"]
+        status = poll["status"] == "open" ? 0 : 1
+        visibility = poll["public"] == "t" ? 1 : 0
+        close_at = (Time.zone.parse(poll["close"]) rescue nil)
+        min = poll["min"].to_i
+        max = poll["max"].to_i
+        step = poll["step"].to_i
 
-          poll_id = execute(<<~SQL
-            INSERT INTO polls (
-              post_id,
-              name,
-              type,
-              status,
-              visibility,
-              close_at,
-              min,
-              max,
-              step,
-              created_at,
-              updated_at
-            ) VALUES (
-              #{r.post_id},
-              '#{escape(name)}',
-              #{type},
-              #{status},
-              #{visibility},
-              #{close_at ? "'#{close_at}'" : "NULL"},
-              #{min > 0 ? min : "NULL"},
-              #{max > min ? max : "NULL"},
-              #{step > 0 ? step : "NULL"},
-              '#{r.created_at}',
-              '#{r.updated_at}'
-            ) RETURNING id
-          SQL
-          )[0]["id"]
+        poll_id = execute(<<~SQL
+          INSERT INTO polls (
+            post_id,
+            name,
+            type,
+            status,
+            visibility,
+            close_at,
+            min,
+            max,
+            step,
+            created_at,
+            updated_at
+          ) VALUES (
+            #{r.post_id},
+            '#{escape(name)}',
+            #{type},
+            #{status},
+            #{visibility},
+            #{close_at ? "'#{close_at}'" : "NULL"},
+            #{min > 0 ? min : "NULL"},
+            #{max > min ? max : "NULL"},
+            #{step > 0 ? step : "NULL"},
+            '#{r.created_at}',
+            '#{r.updated_at}'
+          ) RETURNING id
+        SQL
+        )[0]["id"]
 
-          option_ids = Hash[*DB.query_single(<<~SQL
-            INSERT INTO poll_options
-              (poll_id, digest, html, created_at, updated_at)
+        option_ids = Hash[*DB.query_single(<<~SQL
+          INSERT INTO poll_options
+            (poll_id, digest, html, created_at, updated_at)
+          VALUES
+            #{poll["options"].map { |option| "(#{poll_id}, '#{escape(option["id"])}', '#{escape(option["html"].strip)}', '#{r.created_at}', '#{r.updated_at}')" }.join(",")}
+          RETURNING digest, id
+        SQL
+        )]
+
+        if votes[name].present?
+          execute <<~SQL
+            INSERT INTO poll_votes
+              (poll_id, poll_option_id, user_id, created_at, updated_at)
             VALUES
-              #{poll["options"].map { |option| "(#{poll_id}, '#{escape(option["id"])}', '#{escape(option["html"].strip)}', '#{r.created_at}', '#{r.updated_at}')" }.join(",")}
-            RETURNING digest, id
+              #{votes[name]
+                  .map do |user_id, options|
+                    options
+                      .select { |o| option_ids.has_key?(o) }
+                      .map { |o| "(#{poll_id}, #{option_ids[o]}, #{user_id.to_i}, '#{r.created_at}', '#{r.updated_at}')" }
+                  end.flatten.join(",")
+              }
           SQL
-          )]
-
-          if votes[name].present?
-            execute <<~SQL
-              INSERT INTO poll_votes
-                (poll_id, poll_option_id, user_id, created_at, updated_at)
-              VALUES
-                #{votes[name]
-                    .map do |user_id, options|
-                      options
-                        .select { |o| option_ids.has_key?(o) }
-                        .map { |o| "(#{poll_id}, #{option_ids[o]}, #{user_id.to_i}, '#{r.created_at}', '#{r.updated_at}')" }
-                    end.flatten.join(",")
-                }
-            SQL
-          end
         end
-      rescue
-        Rails.logger.warn "Could not migrate polls for post ##{r.post_id}"
       end
     end
 
